43de49b7ffaad0e9743c4dfbb7ec5815531b1125
s012510 <lunin.development@gmail.com>
2021-05-13 14:37:11 +0300

    strana.ua worked

--- strana_art.py
+++ strana_art.py
@@ -2,7 +2,8 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import re
 
-articles_link = "https://strana.ua/articles.html"
+url_site = "https://strana.ua"
+articles_link = url_site + "/articles.html"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
 
@@ -24,63 +25,21 @@ def getArticleInfo(pageUrl):
 	try:
 		articles = bsObj.find_all('article', {'class': 'lenta-news'})
 		for article in articles:
-			# print(type(article.children))
-			articleImg = None
-			articleTitle = None
-			articleSubTitle = None
-			dateTime = None
-			for articlesTags in article.children:
-				# print(articlesTags.name)
-				# print(articlesTags)
-				try:
-					# del(articlesTags.div.a.i)
-					# print(dir(articlesTags.div.a))
-					# print(articlesTags['class'])
-					if articleImg == None:
-						articleImg = getTagValue(articlesTags.a.img['data-src'])
-					if articleTitle == None:
-						articleTitle = getTagValue(articlesTags.find('a', {'class': 'article'}).contents[0])
-					if articleSubTitle == None:
-						articleSubTitle = getTagValue(articlesTags.find('div', {'class': 'subtitle'}).contents[0])
-					if dateTime == None:
-						# dateTime = getTagValue(articlesTags.time.span['data-time'])
-						dateTime = getTagValue(articlesTags.find('span', {'class': 'strana-adate'})['data-time'])
-					# for realTag in articlesTags.children:
-					# 	try:
-					# 		# print(realTag)
-					# 		# if realTag['class'][0] == 'date':
-					# 		# 	print(realTag['class'][0])
-					# 		# 	print(realTag.contents[1]['data-time'])
-					# 		# 	print(realTag.contents[0])
-					# 		# print(realTag)
-					# 		print('-' * 5)
-					# 	except Exception as e:
-					# 			pass
-				# 	ahref = articlesTags.find('div', {'class': 'lenta-text'})
-				# 	# print(type(ahref))
-				# 	# print(ahref)
-				except AttributeError as error:
-					# print('Attribute error:', end='')
-					# print(error)
-					pass
-				except Exception as e:
-					print('Other error:', end='')
-					print(e)
+			articleImg = getTagValue(article.find('img')['data-src'])
+			articleTitle = getTagValue(article.find('a', {'class': 'article'}).contents[0])
+			articleHREF = url_site + getTagValue(
+				article.find('a', {'class': 'article'})['href'])
+			articleSubTitle = getTagValue(article.find(
+				'div', {'class': 'subtitle'}).contents[0])
+			dateTime = getTagValue(article.find(
+				'span', {'class': 'strana-adate'})['data-time'])
+			print('Article Link:' + articleHREF)
 			print('Image link:' + articleImg)
 			print('Title:' + articleTitle)
 			print('Subtitle:' + articleSubTitle)
 			print('Date:' + dateTime)
-			# print('-' * 10)
-			# s = ''.join(e for e in article if type(e) is bs4.element.NavigableString)
-			# print(article)
-			# print(article.a.get_text())
-			# articleTitle = article.select('a.article').get_text()
-			# print(articleTitle)
 			print('-' * 30)
-			# break
 	except AttributeError as error:
-		# print('Attribute error:', end='')
-		# print(error)
 		pass
 	except Exception as e:
 		print('Other error:', end='')
